2bc72b75a1c8d13328545bff3f074ecd672f33f9
Viktoriia Baraniuk <v.y.baranyk@gmail.com>
2021-02-07 03:09:05 +0200

    Added changes

--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -2,7 +2,7 @@ import s from './GoBackButton.module.css';
 
 export default function GoBackButton({ onClick }) {
   return (
-    <button type="button" onClick={() => onClick()} className={s.button}>
+    <button type="button" onClick={onClick} className={s.button}>
       Go back
     </button>
   );
